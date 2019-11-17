822ec6fd48d3722d6b603cecdcb8562c8c458abd
Lukasz <zakraca@gmail.com>
2019-11-17 19:14:53 +0100

    pytest

---
+++ tests/conftest.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import os
+from time import sleep
+import docker
+import pytest
+from tests.fixtures.driver import *
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+BROWSER = 'chrome'
+SELENIUM_VERSION = '3.141.0'
+SELENIUM_IMAGE_NAME = 'selenium/standalone-{browser}:{version}'.format(
+    browser=BROWSER,
+    version=SELENIUM_VERSION)
+SELENIUM_CONTAINER_NAME = 'standalone-{}'.format(BROWSER)
+
+
+def create_selenium_container(client, network, links=((),)):
+    return client.containers.run(
+        image=SELENIUM_IMAGE_NAME,
+        name=SELENIUM_CONTAINER_NAME,
+        ports={'4444': '4444'},
+        detach=True,
+        networks=[network],
+        volumes={
+            '/dev/shm': {'bind': '/dev/shm', 'mode': 'rw'}
+        },
+        links=links,
+        environment={'GRID_TIMEOUT': '10'})
+
+
+def create_webserver_container(client, network):
+    return client.containers.run(
+        image='python:3.6.1',
+        name='webserver',
+        hostname='webserver',
+        ports={'8000': '8000'},
+        detach=True,
+        networks=[network],
+        volumes={
+            PROJECT_DIR + '/tests/index.html': {
+                'bind': '/www/index.html',
+                'mode': 'ro'
+            }
+        },
+        command="""
+            bash -c "cd /www && python -m http.server"
+            """)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def create_containers_for_tests():
+    if 'TRAVIS' in os.environ:
+        yield
+        return
+    client = docker.from_env()
+    network = client.networks.create("network1", driver="bridge")
+    webserver_container = create_webserver_container(client, network.name)
+    selenium_container = create_selenium_container(
+        client,
+        network.name,
+        links=(('webserver', 'webserver'),))
+    sleep(1)  # Give Selenium some time to wake up.
+    yield
+    selenium_container.remove(force=True)
+    webserver_container.remove(force=True)
+    network.remove()
--- tests/test_items_list.py
+++ tests/test_items_list.py
@@ -1,103 +1,94 @@
-import unittest
-from os import path
-
-from selenium.webdriver import Chrome, ChromeOptions
+import pytest
 
 from items_list import Item
 from items_list.container import Container
 from tests.page_object import HomePage
 
-URL = path.join(path.abspath(path.join(path.dirname(__file__))), 'index.html')
+
+@pytest.fixture(scope='module')
+def home_page(driver):
+    return HomePage(driver, 'http://webserver:8000').open()
 
 
-class TestMockServer:
-    pass
+def test_get_headers(home_page):
+    expected_headers = ['Last Name', 'First Name', 'Email', 'Due', 'Web Site', 'Action']
+    assert home_page.items_list.headers == expected_headers
 
 
-class TestItemsList(unittest.TestCase):
+def test_num_of_items(home_page):
+    assert home_page.items_list.num_rows == 4
 
-    def setUp(self):
-        options = ChromeOptions()
-        options.headless = True
-        self.driver = Chrome(options=options)
-        self.page = HomePage(self.driver, URL)
-        self.page.open()
 
-    def test_get_headers(self):
-        expected_headers = ['Last Name', 'First Name', 'Email', 'Due', 'Web Site', 'Action']
+def test_get_item_by_row_id_1(home_page):
+    item = home_page.items_list.get_item_by_position(1)
 
-        self.assertEqual(self.page.items_list.headers, expected_headers)
+    assert item.first_name.text == 'John'
+    assert item.last_name.text == 'Smith'
+    assert item.email.text == 'jsmith@gmail.com'
+    assert home_page.items_list.current_item == 1
 
-    def test_num_of_items(self):
-        self.assertEqual(self.page.items_list.num_rows, 4)
 
-    def test_get_item_by_row_id_1(self):
-        item = self.page.items_list.get_item_by_position(1)
+def test_get_item_by_row_id_2(home_page):
+    item = home_page.items_list.get_item_by_position(2)
 
-        self.assertEqual(item.first_name.text, 'John')
-        self.assertEqual(item.last_name.text, 'Smith')
-        self.assertEqual(item.email.text, 'jsmith@gmail.com')
-        self.assertEqual(1, self.page.items_list.current_item)
+    assert item.first_name.text == 'Frank'
+    assert item.last_name.text == 'Bach'
+    assert item.email.text == 'fbach@yahoo.com'
+    assert 2 == home_page.items_list.current_item
 
-    def test_get_item_by_row_id_2(self):
-        item = self.page.items_list.get_item_by_position(2)
 
-        self.assertEqual(item.first_name.text, 'Frank')
-        self.assertEqual(item.last_name.text, 'Bach')
-        self.assertEqual(item.email.text, 'fbach@yahoo.com')
-        self.assertEqual(2, self.page.items_list.current_item)
+def test_get_item_by_row_id_3(home_page):
+    item = home_page.items_list[2]
 
-    def test_get_item_by_row_id_3(self):
-        item = self.page.items_list[2]
+    assert item.first_name.text == 'Frank'
+    assert item.last_name.text == 'Bach'
+    assert item.email.text == 'fbach@yahoo.com'
+    assert 2 == home_page.items_list.current_item
 
-        self.assertEqual(item.first_name.text, 'Frank')
-        self.assertEqual(item.last_name.text, 'Bach')
-        self.assertEqual(item.email.text, 'fbach@yahoo.com')
-        self.assertEqual(2, self.page.items_list.current_item)
 
-    def test_get_item_by_property_name_one_property(self):
-        item = self.page.items_list.get_item_by_property(last_name='Doe')
+def test_get_item_by_property_name_one_property(home_page):
+    item = home_page.items_list.get_item_by_property(last_name='Doe')
 
-        self.assertIsNotNone(item)
-        self.assertEqual(item.first_name.text, 'Jason')
-        self.assertEqual(item.last_name.text, 'Doe')
-        self.assertEqual(item.email.text, 'jdoe@hotmail.com')
-        self.assertEqual(item.due.text, '$100.00')
-        self.assertEqual(3, self.page.items_list.current_item)
+    assert item is not None
+    assert item.first_name.text == 'Jason'
+    assert item.last_name.text == 'Doe'
+    assert item.email.text == 'jdoe@hotmail.com'
+    assert item.due.text == '$100.00'
+    assert 3 == home_page.items_list.current_item
 
-    def test_get_item_by_property_name_two_properties(self):
-        item = self.page.items_list.get_item_by_property(last_name='Doe', first_name='Jason')
 
-        self.assertIsNotNone(item)
-        self.assertEqual(item.first_name.text, 'Jason')
-        self.assertEqual(item.last_name.text, 'Doe')
-        self.assertEqual(item.email.text, 'jdoe@hotmail.com')
-        self.assertEqual(item.due.text, '$100.00')
-        self.assertEqual(3, self.page.items_list.current_item)
+def test_get_item_by_property_name_two_properties(home_page):
+    item = home_page.items_list.get_item_by_property(last_name='Doe', first_name='Jason')
 
-    def test_get_item_by_property_name_not_match(self):
-        item = self.page.items_list.get_item_by_property(last_name='Doe', first_name='not match')
-        self.assertIsNone(item)
+    assert item is not None
+    assert item.first_name.text == 'Jason'
+    assert item.last_name.text == 'Doe'
+    assert item.email.text == 'jdoe@hotmail.com'
+    assert item.due.text == '$100.00'
+    assert 3 == home_page.items_list.current_item
 
-    def test_locators(self):
-        locators = self.page.items_list.item.locators
-        self.assertIn('last_name', locators.keys())
-        self.assertIn('first_name', locators.keys())
-        current_item = self.page.items_list.current_item
-        self.assertEqual(locators['last_name'], ("css selector", "tr:nth-of-type({}) td:nth-of-type(1)".format(current_item)))
 
-    def test_implementation_exception(self):
-        class RowItem(Item):
-            locators_template = {'name': ('xpath', '//div')}
+def test_get_item_by_property_name_not_match(home_page):
+    item = home_page.items_list.get_item_by_property(last_name='Doe', first_name='not match')
+    assert item is None
 
-        class Table(Container):
-            item = RowItem()
 
-        table = Table('xpath', '//table')
-        with self.assertRaises(NotImplementedError):
-            table.get_headers_locator()
-            table.get_row_locator()
+def test_locators(home_page):
+    locators = home_page.items_list.item.locators
+    assert 'last_name' in locators.keys()
+    assert 'first_name' in locators.keys()
+    current_item = home_page.items_list.current_item
+    assert locators['last_name'] == ("css selector", "tr:nth-of-type({}) td:nth-of-type(1)".format(current_item))
 
-    def tearDown(self):
-        self.driver.close()
-        self.driver.quit()
+
+def test_implementation_exception():
+    class RowItem(Item):
+        locators_template = {'name': ('xpath', '//div')}
+
+    class Table(Container):
+        item = RowItem()
+
+    table = Table('xpath', '//table')
+    with pytest.raises(NotImplementedError):
+        table.get_headers_locator()
+        table.get_row_locator()
