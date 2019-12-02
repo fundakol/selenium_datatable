b2ccc149d39f8d7edad6a7ca52f97c4be456317f
Lukasz <zakraca@gmail.com>
2019-12-02 19:25:42 +0100

    updated page_object

--- tests/page_object.py
+++ tests/page_object.py
@@ -4,24 +4,24 @@ from selenium.webdriver.common.by import By
 
 class UserItem(RowItem):
     locators_template = {
-        'last_name': (By.CSS_SELECTOR, "tr:nth-of-type({item_num}) td:nth-of-type(1)"),
-        'first_name': (By.CSS_SELECTOR, "tr:nth-of-type({item_num}) td:nth-of-type(2)"),
-        'email': (By.CSS_SELECTOR, "tr:nth-of-type({item_num}) td:nth-of-type(3)"),
-        'due': (By.CSS_SELECTOR, "tr:nth-of-type({item_num}) td:nth-of-type(4)"),
-        'web_site': (By.CSS_SELECTOR, "tr:nth-of-type({item_num}) td:nth-of-type(5)"),
-        'delete_button': (By.CSS_SELECTOR, "tr:nth-of-type({item_num}) td:nth-of-type(6) a[href='#delete']"),
-        'edit_button': (By.CSS_SELECTOR, "tr:nth-of-type({item_num}) td:nth-of-type(6) a[href='#edit']"),
+        'last_name': (By.CSS_SELECTOR, "tr:nth-of-type({row}) td:nth-of-type(1)"),
+        'first_name': (By.CSS_SELECTOR, "tr:nth-of-type({row}) td:nth-of-type(2)"),
+        'email': (By.CSS_SELECTOR, "tr:nth-of-type({row}) td:nth-of-type(3)"),
+        'due': (By.CSS_SELECTOR, "tr:nth-of-type({row}) td:nth-of-type(4)"),
+        'web_site': (By.CSS_SELECTOR, "tr:nth-of-type({row}) td:nth-of-type(5)"),
+        'delete_button': (By.CSS_SELECTOR, "tr:nth-of-type({row}) td:nth-of-type(6) a[href='#delete']"),
+        'edit_button': (By.CSS_SELECTOR, "tr:nth-of-type({row}) td:nth-of-type(6) a[href='#edit']"),
     }
 
 
-class UserItems(Container):
+class UsersTable(Container):
     rows_locator = (By.CSS_SELECTOR, "tbody > tr")
     headers_locator = (By.CSS_SELECTOR, "thead > tr > th")
     item = UserItem()
 
 
 class HomePage:
-    items_list = UserItems(By.ID, "table1")
+    items_list = UsersTable(By.ID, "table1")
 
     def __init__(self, driver, url):
         self.driver = driver
@@ -33,7 +33,7 @@ class HomePage:
 
 
 class NoTablePage:
-    items_list = UserItems(By.ID, "table_does_not_exist")
+    items_list = UsersTable(By.ID, "table_does_not_exist")
 
     def __init__(self, driver, url):
         self.driver = driver
@@ -42,3 +42,7 @@ class NoTablePage:
     def open(self):
         self.driver.get(self.url)
         return self
+
+
+class NoDriverPage:
+    users_table = UsersTable(By.ID, "table1")
