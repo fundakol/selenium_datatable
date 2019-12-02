69d0511b18ca3ca9e021d24338799f2f7da0edcd
Lukasz <zakraca@gmail.com>
2019-12-02 19:37:01 +0100

    updated setup: supporting Python 3.6, 3.7, 3.8

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ setuptools.setup(
 
 classifiers = [
     'Development Status :: 5 - Production/Stable',
-    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
 ]
