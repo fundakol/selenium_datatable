a113f722d44ec8dc6a815478b574eb7f03c9de94
fundakol <fundakol@yahoo.com>
2021-09-11 12:24:44 +0200

    added setup.cfg, pyproject.toml #none

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='selenium_datatable',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    author='Łukasz Fundakowski',
-    author_email='fundakol@yahoo.com',
-    description="A small library for simplifying a table object in selenium",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/fundakol/selenium_datatable',
-    packages=['selenium_datatable'],
-    python_requires='>=3.6',
-    install_requires=['selenium'],
-    keywords='selenium table',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-    ]
-)
+setuptools.setup()
